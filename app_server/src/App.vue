3acc496d22d01fe44325cc9c5da39a3254ad0ca3
LuisRamos99 <48031198+LuisRamos99@users.noreply.github.com>
2021-06-18 18:14:09 +0100

    adicionado algumas dependências e  também já tem login/register

--- app_server/src/App.vue
+++ app_server/src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="app">
+    <Navbar/>
+    <router-view/>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap');
+
+html, body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, Helvetica, sans-serif;
+  height: 64px;
+}
+
+body {
+    height: 100vh;
+} 
+
+html { overflow-y: hidden }
+
+.dropdown {
+  overflow: hidden;
+}
+
+.bigtext{
+  font-family: "Arial Black", "Arial Bold", Gadget, sans-serif;
+  width: 700px;
+  font-size: 25px;
+  margin: 0px 0px 40px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #575d65;
+
+}
+.frontpage{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: auto;
 }
 </style>
---
+++ app_server/src/components/Navbar.vue
@@ -0,0 +1,133 @@
+<template>
+  <div id="navbar-max">
+    <div v-if="token">
+      <v-layout row justify-center class="navbar">
+        <v-toolbar color="#57a2bf" class="hidden-xs-and-down">
+          <v-img
+          @click="goToLink('/')"
+          class="logo"
+          max-height="45"
+          max-width="490"
+          src="../assets/mfc.png"
+          ></v-img>
+            <v-toolbar-items>
+             <v-btn class="navbar-button">
+                <span class="hyperlink" @click="goToLink('/publicacoes')"><b>Publicações</b></span>
+             </v-btn>
+             <v-btn class="navbar-button">
+                <span class="hyperlink" @click="goToLink('/receitas')"><b>Receitas</b></span>
+             </v-btn>
+            </v-toolbar-items>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+             <v-btn class="navbar-button">
+                <span class="hyperlink" @click="goToLink('/perfil')"><b>Perfil</b></span>
+             </v-btn>
+             <v-btn class="navbar-button">
+                <span @click="handleLogout()"> <b> Logout </b></span>
+             </v-btn>
+            </v-toolbar-items>
+          </v-toolbar>
+      </v-layout>
+    </div>
+    <div v-else>
+      <v-layout row justify-center class="navbar">
+        <v-toolbar color="#57a2bf" class="hidden-xs-and-down">
+          <v-img
+          @click="goToLink('/')"
+          class="logo"
+          max-height="45"
+          max-width="490"
+          src="../assets/mfc.png"
+          ></v-img>
+              <v-spacer></v-spacer>
+            <v-toolbar-items>
+             <v-btn class="navbar-button">
+                <Login/>
+             </v-btn>
+            </v-toolbar-items>
+          </v-toolbar>
+      </v-layout>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Login from "@/components/Login.vue"
+//import axios from 'axios'
+
+export default {
+  name: "App",
+  components: {
+    Login
+  },
+  methods: {
+    handleLogout() {
+      localStorage.clear();
+      this.$router.go()
+      window.location.href = '/'
+    },
+    goToLink(link) {
+      var currentUrl = window.location.pathname;
+      if (currentUrl!=link) this.$router.push(link)
+    }
+  },
+  data () {
+    return {
+      idUser: null,
+      dialog: false,
+      token: ''
+    }
+  },
+  created() {
+    /*
+    if (this.token) {
+      axios({
+          method: "post",
+          url: "http://localhost:8081/api/user/token/",
+          data: this.token,
+      })
+      .then(data => {
+          this.idUser = data.data.idUser
+          this.link = this.link + this.idUser
+      })
+      .catch(err => {
+          console.log(err)
+      })
+    }*/
+  }
+};
+</script>
+
+<style>
+
+#navbar-max {
+    margin-bottom: 70px;
+}
+
+.navbar {
+    z-index: 10; 
+    width:100%; 
+    position: fixed;
+    margin-left: 0px !important;
+}
+
+.navbar-button {
+    background-color: #57a2bf !important; 
+    padding-top: 10px !important; 
+    box-shadow: 0px 0px 0px 0px rgb(0 0 0 / 20%) !important;
+}
+
+.hyperlink {
+    text-decoration: none;
+    color: #212121 !important;
+}
+
+.logo{
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-top: 10px;
+}
+</style>
+
