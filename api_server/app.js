b75b72f75e6a4a4f13bbe1dce38984f95b1a6064
Jcc20 <48126929+Jcc20@users.noreply.github.com>
2021-06-18 20:30:30 +0100

    Login e Registar feito

--- api_server/app.js
+++ api_server/app.js
@@ -3,8 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/utilizador');
 
 var app = express();
 
@@ -14,7 +15,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+app.use('/utilizador', usersRouter);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
---
+++ api_server/routes/utilizador.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var router = express.Router();
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken')
+var axios = require('axios')
+var gdb = require("../utils/graphdb");
+
+var prefixes = `
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX xml: <http://www.w3.org/XML/1998/namespace>
+    PREFIX : <http://www.di.uminho.pt/prc2021/PRC2021_Tp#> 
+`
+function verifyToken(token){  
+  token = token.split(" ")[1] // Bearer ey341...
+  var t = null;
+  
+  jwt.verify(token,'PRC2021',function(e,decoded){
+    if(e){
+      t = null
+    }
+    else return t = decoded
+  })
+
+  return t
+}
+
+router.get('/login' ,async function(req, res){
+  var query = `SELECT * WHERE {	<http://www.di.uminho.pt/prc2021/PRC2021_Tp#${req.body.email}> :password ?p. 
+  <http://www.di.uminho.pt/prc2021/PRC2021_Tp#${req.body.email}> :nome ?n.} `;
+
+  try{
+    var dados = await gdb.execQuery(query);
+      
+    if(!dados.results.bindings[0]) { return res.status(500).jsonp({message:"Utilizador inexistente!\n"})}
+    if(req.body.password!=dados.results.bindings[0].p.value){
+      return res.status(403).jsonp({message:"Credenciais inválidas!\n"})
+    }
+  
+    jwt.sign({email: req.body.email, nome: dados.results.bindings[0].n.value},
+      'PRC2021',
+      {expiresIn: "3h"},
+      function(e, token) {
+        if (e) res.status(500).jsonp({error:"Erro na geração do token: " + e})
+        else res.status(201).jsonp({token: token})
+    });
+
+  }catch(err){
+    res.status(500).jsonp({error:"Erro no login:"+ err})
+  }
+
+})
+
+
+router.post('/registar' , async function(req, res){
+  var query = `INSERT DATA
+  { 
+  <http://www.di.uminho.pt/prc2021/PRC2021_Tp#${req.body.email}> rdf:type :Utilizador;
+                                                 :nome "${req.body.nome}" ;
+                                                 :email "${req.body.email}" ;
+                                                 :password "${req.body.password}".
+  }`
+  console.log(query)
+
+  var result = await gdb.execTransaction(query);
+  res.status(201).jsonp({message: "Utilizador registado com sucesso!"})
+  
+})
+
+
+module.exports = router;
