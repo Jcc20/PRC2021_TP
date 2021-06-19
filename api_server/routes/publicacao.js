2b219a1326bb83380bf93c1cddccc5ab5a7935b1
Jcc20 <48126929+Jcc20@users.noreply.github.com>
2021-06-19 21:45:09 +0100

    more routes

--- api_server/routes/publicacao.js
+++ api_server/routes/publicacao.js
@@ -18,61 +18,40 @@ function verifyToken(token){
   return t
 }
 
-router.get('/', async function(req, res, next) {
-    var query = `select  ?s ?d ?da ?di ?tc ?tp (GROUP_CONCAT(distinct ?ig;SEPARATOR="&") AS ?igs) ?t (GROUP_CONCAT(distinct ?g;SEPARATOR="&") AS ?gs) where {  ?s rdf:type :Receita.
+router.get('/receita/:id', async function(req, res, next) {
+    var query = `select   ?s ?d ?da ?t ?tr ?a ?n where {  ?s rdf:type :Publicacao.
+        ?s :RelativaA :${req.params.id}.
         ?s :descricao ?d.
         ?s :data ?da.
-        ?s :dificuldade ?di.
-        ?s :ingrediente ?ig.
    		?s :titulo ?t.
-        ?s :tipoCozinha ?tc.
-        ?s :tipoPrato ?tp.
-        OPTIONAL {?s :éGostadoPor ?g.}`
-           
-    if(req.query.titulo){
-        query+=`\n?s :titulo ?r.
-        FILTER regex (str(?r), "${req.query.titulo}", "i").`
-    }
-    else if(req.query.tipoCozinha){
-        query+="\n?s :tipoCozinha \""+req.query.tipoCozinha+"\"."
-    }else if(req.query.tipoPrato){
-        query+="\n?s :tipoPrato \""+req.query.tipoPrato+"\"."
-    }else if(req.query.ingrediente){
-        query+=`\n?s :ingrediente ?i.
-        FILTER regex (str(?i), "${req.query.ingrediente}", "i").`
-    }else if(req.query.autor){
-        query+=`\n?s :CriadaPor ?c.
-    	?c :nome "${req.query.autor}".`
-    }
-    query+="} group by ?s ?d ?da ?di ?t ?tc ?tp"
+        :${req.params.id}  :titulo ?tr.
+   		?s :CriadaPor ?a.
+        ?a :nome ?n.
+       }
+       `
+        
     console.log(query)
     var result =await gdb.execQuery(query)
 
     var arr = []
-    result.results.bindings.forEach(a => {
-        var ing=[]
-        a.igs.value.split('&').forEach(i=>{
-            ing.push(i)
-        })
-        var gostos = []
-        a.gs.value.split('&').forEach(g=>{
-            console.log(g)
-           gostos.push(g.split('#')[1])
-        })
-        var obj = {
-            "rec_id": a.s.value.split('#')[1],
-            "descricao": a.d.value,
-            "ingredientes": ing,
-            "titulo": a.t.value,
-            "dificuldade": a.di.value,
-            "gostos": gostos,
-            "data": a.da.value,
-            "tipoCozinha": a.tc.value,
-            "tipoPrato": a.tp.value
-        }
-        arr.push(obj)
-    });
-    res.status(201).jsonp({receitas:arr})
+    if(result.results.bindings[0]){
+        result.results.bindings.forEach(a => {
+            var obj = {
+                "pub_id": a.s.value.split('#')[1],
+                "descricao": a.d.value,
+                "titulo": a.t.value,
+                "data": a.da.value,
+                "titulo_receita": a.tr.value,
+                "autor_id": a.a.value.split('#')[1],
+                "autor": a.n.value
+            }      
+            arr.push(obj)
+        });   
+        res.status(201).jsonp({publi:arr})
+    }else{
+        res.status(404).jsonp({message:"Não existem publicações!"})
+
+    }
 });
 
 
