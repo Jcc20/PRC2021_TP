7b3766f77d9e511878ff34db276150b2383ff968
LuisRamos99 <48031198+LuisRamos99@users.noreply.github.com>
2021-06-19 23:06:53 +0100

    adicionar receitas feito

--- app_server/src/components/AddReceita.vue
+++ app_server/src/components/AddReceita.vue
@@ -23,67 +23,63 @@
                             ></v-text-field>
                         </v-col>
 
-                        <v-col class="pa-2">
-                            <v-text-field 
+
+                        <v-col class="pa-2" style="margin-top:-5px">
+                            <v-autocomplete
+                              v-model="tipoCozinha"
+                              :items="tiposCozinha"
+                              outlined
+                              dense
+                              label="Tipo de cozinha"
+                            ></v-autocomplete>
+                        </v-col>
+
+                        <v-col class="pa-2" style="margin-top:-30px">
+                            <v-autocomplete
+                              v-model="tipoPrato"
+                              :items="tiposPrato"
+                              outlined
+                              dense
+                              label="Tipo de prato"
+                            ></v-autocomplete>
+                        </v-col>
+
+                        <v-col class="pa-2" style="margin-top:-30px">
+                            <v-select
+                              v-model="dificuldade"
+                              :items="dificuldades"
+                              outlined
+                              dense
+                              label="Dificuldade do prato"
+                            ></v-select>
+                        </v-col>
+
+                        <v-col class="pa-2" style="margin-top:-30px">
+                          <v-combobox 
+                            multiple
+                            v-model="select" 
+                            label="Ingredientes" 
+                            chips
+                            outlined
+                            deletable-chips
+                            class="tag-input"
+                            :search-input.sync="search" 
+                            @keyup.tab="updateTags"
+                            @paste="updateTags">
+                          </v-combobox>
+                        </v-col>
+                      
+                        <v-col class="pa-2" style="margin-top:-35px">
+                            <v-textarea 
                             hide-details
                             dense
                             type="text" 
                             v-model="descricao" 
                             label="Descrição"
                             outlined
-                            ></v-text-field>
+                            ></v-textarea>
                         </v-col>
 
-                        <v-col class="pa-2" style="margin-bottom:-25px">
-                            <v-autocomplete
-                              v-model="tipo"
-                              :items="types"
-                              outlined
-                              dense
-                              label="Seleciona o tipo"
-                            ></v-autocomplete>
-                        </v-col>
-
-                        <v-col class="pa-2">
-                            <v-menu
-                              ref="menu"
-                              v-model="menu"
-                              :close-on-content-click="false"
-                              transition="scale-transition"
-                              offset-y
-                              min-width="auto"
-                            >
-                                <template v-slot:activator="{ on, attrs }">
-                                  <v-text-field
-                                    v-model="data"
-                                    hide-details
-                                    dense
-                                    outlined
-                                    label="Data de criação"
-                                    readonly
-                                    v-bind="attrs"
-                                    v-on="on"
-                                   ></v-text-field>
-                                </template>
-
-                                <v-date-picker
-                                  ref="picker"
-                                  v-model="data"
-                                  color="#4F4E81"
-                                  :max="new Date().toISOString().substr(0, 10)"
-                                  min="1900-01-01"
-                                  @change="save"
-                                ></v-date-picker>
-                            </v-menu>
-                        </v-col>
-                      
-                        <v-col cols="1" offset="11">
-                            <v-file-input
-                              v-model="fileInput"
-                              hide-input
-                              prepend-icon="mdi-plus"
-                            ></v-file-input>
-                        </v-col>
                         
                         <v-col align="right">
                           <v-btn :loading="loading" v-ripple="{ class: 'primary--text' }" width="150" style="height:40px" class="white--text" elevation="1" v-on:click="submeter()" color="#00ace6">Submeter</v-btn>
@@ -107,13 +103,19 @@ export default {
         return{
           hover: false,
           show:false,
-          types:[],
+          search: "",
+          select: [],
+          tiposCozinha: ["marroquina","chinesa","italiana","baguete"],
+          tiposPrato: ["carne","peixe","sobremesa","pizza"],
+          dificuldades: ["Fácil","Média","Difícil"],
+          dificuldade:'',
           titulo:'',
           descricao:'',
           data:null,
           menu: false,
           loading:false,
-          tipo:'',
+          tipoPrato:'',
+          tipoCozinha:'',
           fileInput:''
         }
     },
@@ -129,6 +131,14 @@ export default {
         },
         save(date) {
             this.$refs.menu.save(date)
+        },
+        updateTags() {
+          this.$nextTick(() => {
+            this.select.push(...this.search.split(","));
+            this.$nextTick(() => {
+              this.search = "";
+            });
+          });
         },
         submeter() {
             this.loading=true
