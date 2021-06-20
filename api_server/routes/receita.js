027d63d635d580f9bb73a889d6049cc499c1b953
Jcc20 <48126929+Jcc20@users.noreply.github.com>
2021-06-20 17:00:20 +0100

    Update receita.js

--- api_server/routes/receita.js
+++ api_server/routes/receita.js
@@ -115,46 +115,51 @@ limit 6`
 });
 
 router.post('/', async function(req, res, next) {
-    var rec_id = uuidv4()
-    var ing = ""
-    var poped =  req.body.ingredientes.pop()
-    req.body.ingredientes.forEach(i => {
-        ing+='"'+i+'"'+",\n"
-    });
-    ing+='"'+poped+'"'+";\n"
+    var token = unveilToken(req.headers.authorization)
+    if(!token || token.email != req.body.idUser) {res.status(403).jsonp({erro: "Não tem acesso à operação."})}
+    else{
 
-    console.log(ing)
-    var query = `INSERT DATA
-    { 
-           :${rec_id} rdf:type :Receita;
-                    :titulo "${req.body.titulo}" ;
-                    :data "${req.body.data}" ;
-                    :descricao "${req.body.descricao}";
-                    :ingrediente ${ing}
-                    :dificuldade "${req.body.dificuldade}";
-                    :tipoCozinha "${req.body.tipoCozinha}";
-                    :tipoPrato "${req.body.tipoPrato}".
-    }`
-    console.log(query)
-    var queryRel = `INSERT 
-    {
-        :${rec_id} :CriadoPor ?p.
-        ?p :Criou  :${rec_id}.
-    } 
-    where{
-        ?p rdf:type :Utilizador .
-        FILTER regex (str(?p), "${req.body.idUser}").
-    }`
-    console.log(queryRel)
-    try {
-        var result =await gdb.execTransaction(query)
-        console.log(result)
-        var resultRel =await gdb.execTransaction(queryRel)
-        console.log(resultRel)
-        
-        res.status(201).jsonp({message:"Receita registada com sucesso!", idRec: rec_id})
-    } catch (error) {
-        res.status(500).jsonp({message:"Erro no registo da receita! "+ error})
+        var rec_id = uuidv4()
+        var ing = ""
+        var poped =  req.body.ingredientes.pop()
+        req.body.ingredientes.forEach(i => {
+            ing+='"'+i+'"'+",\n"
+        });
+        ing+='"'+poped+'"'+";\n"
+    
+        console.log(ing)
+        var query = `INSERT DATA
+        { 
+               :${rec_id} rdf:type :Receita;
+                        :titulo "${req.body.titulo}" ;
+                        :data "${req.body.data}" ;
+                        :descricao "${req.body.descricao}";
+                        :ingrediente ${ing}
+                        :dificuldade "${req.body.dificuldade}";
+                        :tipoCozinha "${req.body.tipoCozinha}";
+                        :tipoPrato "${req.body.tipoPrato}".
+        }`
+        console.log(query)
+        var queryRel = `INSERT 
+        {
+            :${rec_id} :CriadoPor ?p.
+            ?p :Criou  :${rec_id}.
+        } 
+        where{
+            ?p rdf:type :Utilizador .
+            FILTER regex (str(?p), "${req.body.idUser}").
+        }`
+        console.log(queryRel)
+        try {
+            var result =await gdb.execTransaction(query)
+            console.log(result)
+            var resultRel =await gdb.execTransaction(queryRel)
+            console.log(resultRel)
+            
+            res.status(201).jsonp({message:"Receita registada com sucesso!", idRec: rec_id})
+        } catch (error) {
+            res.status(500).jsonp({message:"Erro no registo da receita! "+ error})
+        }
     }
 
 });
